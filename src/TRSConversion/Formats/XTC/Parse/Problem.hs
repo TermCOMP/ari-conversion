d36418010aaf1abdbfef9d69af9fa9f4a6fb8905
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-03-22 11:03:46 +0100

    support for context sensitive systems

--- src/TRSConversion/Formats/XTC/Parse/Problem.hs
+++ src/TRSConversion/Formats/XTC/Parse/Problem.hs
@@ -2,41 +2,55 @@ module TRSConversion.Formats.XTC.Parse.Problem (parse) where
 
 import qualified TRSConversion.Problem.Trs.Trs as Ari
 import qualified TRSConversion.Problem.Problem as Ari.Problem
+import qualified TRSConversion.Problem.CSTrs.CSTrs as Ari.CSTrs
 import qualified TPDB.Data as TPDB
-import qualified TPDB.Input.File as Input
+import qualified TPDB.XTC.Read as Read
 import qualified TRSConversion.Problem.Common.MetaInfo as MetaInfo ( emptyMetaInfo )
 import qualified Data.IntMap as IntMap
 import qualified Data.Text as Text
+import TPDB.Data (Funcsym)
+import Data.Maybe ( isNothing )
 
-type SrcTrs = TPDB.TRS TPDB.Identifier TPDB.Identifier
-type SrcFunctionSymbol = TPDB.Identifier
-type SrcSignature = [SrcFunctionSymbol]
+type SrcProblem = TPDB.Problem TPDB.Identifier TPDB.Identifier
+type SrcFunctionSymbol = Funcsym
+type SrcSignature = TPDB.Signature
 type SrcRule = (SrcTerm, SrcTerm)
 type SrcTerm = TPDB.Term TPDB.Identifier TPDB.Identifier
 
 type DstProblem = Ari.Problem.Problem String String String
-type DstTrs = Ari.Trs String String
 type DstSignature = Ari.TrsSig String
 type DstRule = Ari.Rule String String
 type DstTerm = Ari.Term String String
 type DstFunctionSymbol = Ari.Sig String
+type DstReplacementMap = Ari.CSTrs.ReplacementMap String
 
 parse :: FilePath -> IO (Either String DstProblem)
 parse f = do
-    src_trs <- Input.get_trs f
-    let dst_trs = convertTrs src_trs
-    return $ Right $ Ari.Problem.Problem {
-        Ari.Problem.metaInfo = MetaInfo.emptyMetaInfo,
-        Ari.Problem.system = Ari.Problem.Trs dst_trs}
+    src_problem <- Read.readProblemF f
+    return $ convertProblem src_problem
 
-convertTrs :: SrcTrs -> DstTrs
-convertTrs trs =
-    let rules = map convertRule (TPDB.strict_rules trs)
-        signature = convertSignature $ TPDB.signature trs in
-    Ari.Trs {
-        Ari.rules = IntMap.singleton 0 rules,
-        Ari.signature = signature,
-        Ari.numSystems = 1}
+convertProblem :: SrcProblem -> Either String DstProblem
+convertProblem p = do
+    let src_trs = TPDB.trs p
+    let src_signature = TPDB.full_signature p
+    let dst_rules = map convertRule $ TPDB.strict_rules src_trs
+    dst_signature <- convertSignature src_signature
+    replacement_map <- convertReplacementMap src_signature
+    let system = case replacement_map of
+            Nothing ->
+                Ari.Problem.Trs $ Ari.Trs {
+                    Ari.rules = IntMap.singleton 0 dst_rules,
+                    Ari.signature = dst_signature,
+                    Ari.numSystems = 1}
+            Just rm ->
+                Ari.Problem.CSTrs $ Ari.CSTrs.CSTrs {
+                    Ari.CSTrs.rules = IntMap.singleton 0 dst_rules,
+                    Ari.CSTrs.signature = dst_signature,
+                    Ari.CSTrs.replacementMap = rm,
+                    Ari.CSTrs.numSystems = 1}
+    return $ Ari.Problem.Problem {
+        Ari.Problem.system = system,
+        Ari.Problem.metaInfo = MetaInfo.emptyMetaInfo}
 
 convertRule :: SrcRule -> DstRule
 convertRule (lhs, rhs) = Ari.Rule (convertTerm lhs) (convertTerm rhs)
@@ -45,8 +59,20 @@ convertTerm :: SrcTerm -> DstTerm
 convertTerm (TPDB.Var x) = Ari.Var $ Text.unpack (TPDB.name x)
 convertTerm (TPDB.Node f args) = Ari.Fun (Text.unpack $ TPDB.name f) (map convertTerm args)
 
-convertSignature :: SrcSignature -> DstSignature
-convertSignature fs = Ari.FunSig $ map convertFunctionSymbol fs
+convertSignature :: SrcSignature -> Either String DstSignature
+convertSignature (TPDB.Signature fs) = Right $ Ari.FunSig $ map convertFunctionSymbol fs
+convertSignature TPDB.HigherOrderSignature = Left "higher order is not yet supported"
 
 convertFunctionSymbol :: SrcFunctionSymbol -> DstFunctionSymbol
-convertFunctionSymbol f = Ari.Sig (Text.unpack $ TPDB.name f) (TPDB.arity f)
+convertFunctionSymbol f = Ari.Sig (Text.unpack $ TPDB.fs_name f) (TPDB.fs_arity f)
+
+convertReplacementMap :: SrcSignature -> Either String (Maybe DstReplacementMap)
+convertReplacementMap (TPDB.Signature fs) = Right $
+    if all (isNothing . TPDB.fs_replacementmap) fs then Nothing
+    else
+        Just $ map (\f ->
+            let f' = Text.unpack $ TPDB.fs_name f in
+            case TPDB.fs_replacementmap f of
+                Just (TPDB.Replacementmap m) -> (f',m)
+                Nothing -> (f',[0..TPDB.fs_arity f])) fs
+convertReplacementMap TPDB.HigherOrderSignature = Left "higher order is not yet supported"
--- src/TRSConversion/Formats/XTC/Unparse/Problem.hs
+++ src/TRSConversion/Formats/XTC/Unparse/Problem.hs
@@ -1,6 +1,7 @@
 module TRSConversion.Formats.XTC.Unparse.Problem (unparse) where
 
 import qualified TRSConversion.Problem.Trs.Trs as Ari
+import qualified TRSConversion.Problem.CSTrs.CSTrs as Ari.CSTrs
 import qualified TRSConversion.Problem.Problem as Ari.Problem
 import qualified TPDB.Data as TPDB
 import qualified TPDB.Data.Attributes as TPDB
@@ -9,19 +10,19 @@ import qualified Data.Text as Text
 import qualified TPDB.XTC as XTC
 import Data.IntMap ((!))
 import qualified Prettyprinter (Doc, pretty)
-
+import Data.Foldable (find)
 
 type SrcProblem = Ari.Problem.Problem String String String
-type SrcTrs = Ari.Trs String String
 type SrcSignature = Ari.TrsSig String
 type SrcRule = Ari.Rule String String
 type SrcTerm = Ari.Term String String
 type SrcFunctionSymbol = Ari.Sig String
+type SrcReplacementMap = Ari.CSTrs.ReplacementMap String
 
 type DstProblem = TPDB.Problem TPDB.Identifier TPDB.Identifier
 type DstTrs = TPDB.TRS TPDB.Identifier TPDB.Identifier
-type DstFunctionSymbol = TPDB.Identifier
-type DstSignature = [DstFunctionSymbol]
+type DstFunctionSymbol = TPDB.Funcsym
+type DstSignature = TPDB.Signature
 type DstTerm = TPDB.Term TPDB.Identifier TPDB.Identifier
 type DstRule = TPDB.Rule DstTerm
 
@@ -34,36 +35,38 @@ convertProblem :: SrcProblem -> Either String DstProblem
 convertProblem p =
     case Ari.Problem.system p of
         Ari.Problem.Trs src_trs ->
-            let dst_trs = convertTrs src_trs in
-            let signature = TPDB.signature dst_trs
-                rules = TPDB.rules dst_trs in
-            let full_signature = fullSignature signature
-                attributes = TPDB.compute_attributes rules in
-            Right $ TPDB.Problem {
-                TPDB.type_ = TPDB.Termination,
-                TPDB.trs = dst_trs,
-                TPDB.strategy = Nothing,
-                TPDB.full_signature = full_signature,
-                TPDB.startterm = Nothing,
-                TPDB.attributes = attributes
-            }
+            let src_rules = Ari.rules src_trs ! 1
+                src_signature = Ari.signature src_trs in
+            buildProblem src_signature Nothing src_rules
+        Ari.Problem.CSTrs src_trs ->
+            let src_rules = Ari.CSTrs.rules src_trs ! 1
+                replacement_map = Ari.CSTrs.replacementMap src_trs
+                src_signature = Ari.CSTrs.signature src_trs in
+            buildProblem src_signature (Just replacement_map) src_rules
         _ -> Left "XTC export is not yet supported for the given rewrite system"
 
-fullSignature :: DstSignature -> TPDB.Signature
-fullSignature fs = TPDB.Signature $ map (\x -> TPDB.Funcsym {
-    TPDB.fs_name = TPDB.name x,
-    TPDB.fs_arity = TPDB.arity x,
-    TPDB.fs_theory = Nothing,
-    TPDB.fs_replacementmap = Nothing}) fs
+buildProblem :: SrcSignature -> Maybe SrcReplacementMap -> [SrcRule] -> Either String DstProblem
+buildProblem src_signature replacement_map src_rules = do
+    let dst_rules = map convertRule src_rules
+    let dst_signature = convertSignature replacement_map src_signature
+    trs <- buildTrs dst_signature dst_rules
+    return TPDB.Problem {
+        TPDB.type_ = TPDB.Termination,
+        TPDB.trs = trs,
+        TPDB.strategy = Nothing,
+        TPDB.full_signature = dst_signature,
+        TPDB.startterm = Nothing,
+        TPDB.attributes = TPDB.compute_attributes dst_rules}
 
-convertTrs :: SrcTrs -> DstTrs
-convertTrs trs =
-    let rules = map convertRule (Ari.rules trs ! 1)
-        signature = convertSignature $ Ari.signature trs in
-    TPDB.RS {
-        TPDB.rules = rules,
-        TPDB.signature = signature,
-        TPDB.separate = False}
+buildTrs :: DstSignature -> [DstRule] -> Either String DstTrs
+buildTrs (TPDB.Signature fs) rules = Right $ TPDB.RS {
+    TPDB.rules = rules,
+    TPDB.signature = map (\f -> symbol (Text.unpack $ TPDB.fs_name f) (TPDB.fs_arity f)) fs,
+    TPDB.separate = False}
+buildTrs TPDB.HigherOrderSignature _ = Left "higher order is not yet supported"
+
+symbol :: String -> Int -> TPDB.Identifier
+symbol s a = TPDB.mk a $ Text.pack s
 
 convertRule :: SrcRule -> DstRule
 convertRule (Ari.Rule lhs rhs) = TPDB.Rule {
@@ -73,17 +76,24 @@ convertRule (Ari.Rule lhs rhs) = TPDB.Rule {
     TPDB.top = False,
     TPDB.original_variable = Nothing}
 
-symbol :: String -> Int -> TPDB.Identifier
-symbol s a = TPDB.mk a $ Text.pack s
-
 convertTerm :: SrcTerm -> DstTerm
 convertTerm (Ari.Var x) = TPDB.Var $ symbol x 0
 convertTerm (Ari.Fun name args) =
     let arity = length args in
     TPDB.Node (symbol name arity) (map convertTerm args)
 
-convertSignature :: SrcSignature -> DstSignature
-convertSignature (Ari.FunSig fs) = map convertFunctionSymbol fs
+convertSignature :: Maybe SrcReplacementMap -> SrcSignature -> DstSignature
+convertSignature rm (Ari.FunSig src_fs) = TPDB.Signature $ map (convertFunctionSymbol rm) src_fs
 
-convertFunctionSymbol :: SrcFunctionSymbol -> DstFunctionSymbol
-convertFunctionSymbol (Ari.Sig name arity) = symbol name arity
+convertFunctionSymbol :: Maybe SrcReplacementMap -> SrcFunctionSymbol -> DstFunctionSymbol
+convertFunctionSymbol rm (Ari.Sig name arity) =  TPDB.Funcsym {
+    TPDB.fs_name = Text.pack name,
+    TPDB.fs_arity = arity,
+    TPDB.fs_theory = Nothing,
+    TPDB.fs_replacementmap = getReplacementMap rm name}
+
+getReplacementMap :: Maybe SrcReplacementMap -> String -> Maybe (TPDB.Replacementmap)
+getReplacementMap replacement_map name = do
+    rm <- replacement_map
+    (_,positions) <- find (\(n,_) -> n == name) rm
+    return $ TPDB.Replacementmap positions
