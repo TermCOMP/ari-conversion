f4fe88fedde5c7fd90077dd5e2e2f49ab242ecdb
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2024-04-25 16:00:00 +0200

    quote identifiers that start with numbers, keywords

--- src/TRSConversion/Formats/ARI/Unparse/Problem/Utils.hs
+++ src/TRSConversion/Formats/ARI/Unparse/Problem/Utils.hs
@@ -2,8 +2,13 @@
 module TRSConversion.Formats.ARI.Unparse.Problem.Utils (unparseIdentifier) where
 import Prettyprinter ( Doc, Pretty(pretty) )
 import TRSConversion.Formats.ARI.Parse.Utils (isIdentChar)
+import Data.Char (isDigit)
+
+ariKeywords :: [String]
+ariKeywords = ["fun", "rule", "format", "sort", "theory", "define-fun"]
 
 unparseIdentifier :: Pretty f => f -> Doc ann
 unparseIdentifier identifier =
-    let str = pretty identifier in
-    if all isIdentChar $ show str then str else pretty "|" <> str <> pretty "|"
+    let ident = pretty identifier in
+    let str = show ident in
+    if all isIdentChar str && not (isDigit $ head str) && str `notElem` ariKeywords then ident else pretty "|" <> ident <> pretty "|"
